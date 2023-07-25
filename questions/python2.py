5c5cef3afc11f1e08b7cec60e3a2d64ac050682d
c-wright-98 <corvus.wright@gmail.com>
2023-07-25 15:15:53 +0100

    Completed Question 2

--- questions/python2.py
+++ questions/python2.py
@@ -55,7 +55,13 @@ def one(string):
 
 
 def two(number):
-    return False
+    if number > 1:
+        for i in range(2,int(number/2)+1):
+            if number % i == 0:
+                return False
+        else:
+            return True
+
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------
 
